89d2c3c5bb2b7411904080b2d143981c130d92a1
Anderson Costa <dissu0072@gmail.com>
2022-10-22 03:45:57 -0300

    Imprime numeros de 1 ate 25

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // for (let nomber of numbers){
 //     console.log(nomber);
@@ -58,16 +58,23 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 
-let menor = numbers[0];
+// let menor = numbers[0];
 
-for (let index = 1; index < numbers.length; index += 1){
-    if (numbers[index] < menor) {
-        menor = numbers [index] ;
-    }
-}
+// for (let index = 1; index < numbers.length; index += 1){
+//     if (numbers[index] < menor) {
+//         menor = numbers [index] ;
+//     }
+// }
 
-console.log(menor);
+// console.log(menor);
 
 // Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+numeros = [];
+
+for (index = 1; index < 26; index += 1){
+    numeros.push(index);
+}
+console.log(numeros);
 
 // Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
