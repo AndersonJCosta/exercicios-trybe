8e87f274f0e72b35ad245dc364c92604a8f9bef4
Anderson Costa <dissu0072@gmail.com>
2022-10-24 16:02:50 -0300

    Dez fatorial

---
+++ fundamentos-4/exercicios.js
@@ -0,0 +1,9 @@
+// Com base nessas informações, crie um algoritmo que imprima na tela o fatorial
+
+let dezFatorial = 1;
+
+for (let index = 1; index <= 10; index += 1){
+    dezFatorial = dezFatorial * index
+}
+
+console.log( dezFatorial );
