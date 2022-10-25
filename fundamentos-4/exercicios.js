3bca2dff8e231c92bc8ba38e63c778ad6444e78a
Anderson Costa <dissu0072@gmail.com>
2022-10-25 12:09:37 -0300

    Retorna maior palavra do array

--- fundamentos-4/exercicios.js
+++ fundamentos-4/exercicios.js
@@ -10,9 +10,20 @@
 
 // Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
-let word = 'Anderson Jose Agostinho';
-let temp = [];
-for (let index = 0; index < word.length; index +=1){
-    temp.push(word [word.length-1-index]);
+// let word = 'Anderson Jose Agostinho';
+// let temp = [];
+// for (let index = 0; index < word.length; index +=1){
+//     temp.push(word [word.length-1-index]);
+// }
+// console.log(temp.join(''));
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+// Utilize a estrutura de repetição for para escrever dois algoritmos: um que retorne a maior palavra desse array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+let maior = [];
+for (let index = 0; index < array.length; index += 1 ){
+    if (array[index].length>maior.length){
+        maior = array[index]
+    }
 }
-console.log(temp.join(''));
+
+console.log("A maior palava é: " + maior);
