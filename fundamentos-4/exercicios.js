8e5285bb5fa9fa1ed6803a29471a85ea04a655d1
Anderson Costa <dissu0072@gmail.com>
2022-10-25 14:38:43 -0300

    Maior e menor palavra do array

--- fundamentos-4/exercicios.js
+++ fundamentos-4/exercicios.js
@@ -18,12 +18,23 @@
 // console.log(temp.join(''));
 
 let array = ['java', 'javascript', 'python', 'html', 'css'];
+
 // Utilize a estrutura de repetição for para escrever dois algoritmos: um que retorne a maior palavra desse array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
-let maior = [];
+let maior = array[0];
+let menor = array[0];
+
 for (let index = 0; index < array.length; index += 1 ){
     if (array[index].length>maior.length){
         maior = array[index]
     }
 }
 
-console.log("A maior palava é: " + maior);
+console.log("A maior palava é " + maior);
+
+for (let index = 0; index < array.length; index += 1 ){
+    if (array[index].length < menor.length){
+        menor = array[index]
+    }
+}
+
+console.log(menor);
