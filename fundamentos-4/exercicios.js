e37662d420617b5a9d786628f46614baa919284e
Anderson Costa <dissu0072@gmail.com>
2022-10-26 08:16:09 -0300

    Retorna maior primo ente 2 e 50

--- fundamentos-4/exercicios.js
+++ fundamentos-4/exercicios.js
@@ -16,7 +16,7 @@
 //     temp.push(word [word.length-1-index]);
 // }
 // console.log(temp.join(''));
-
+/*
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 
 // Utilize a estrutura de repetição for para escrever dois algoritmos: um que retorne a maior palavra desse array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
@@ -38,3 +38,23 @@ for (let index = 0; index < array.length; index += 1 ){
 }
 
 console.log(menor);
+*/
+/*
+Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.
+*/
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
