8b99bc9720cc0321005d89be12bf50d2d8753361
Anderson Costa <dissu0072@gmail.com>
2022-10-26 10:28:14 -0300

    Adiciona item a um array dentro de um objeto e mostra quantidade de itens do arry

---
+++ objetos-foncoes/exercicio4.js
@@ -0,0 +1,29 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  //O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+
+console.log();
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+console.log();
+
+leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  });
+
+/*
+Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código.
+*/
+  console.log('Julia tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
